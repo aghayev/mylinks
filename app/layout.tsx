72125a9cab6f67bbaec65205d58494ff20f6916a
Imran Aghayev <imran.aghayev@visiondirect.co.uk>
2024-11-18 12:01:09 +0000

    fix(mylinks-29): updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import styles from './layout.module.css';
 import './globals.css';
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "Mobile App Mylinks",
@@ -18,8 +19,13 @@ export default function RootLayout({
         <title>My Links by Imran Aghayev</title>
       </head>
       <body>
+      <div className={styles.leftPlacement}>
+        <Navbar />
+        </div>
+        <div className={styles.rightPlacement}>
       <Header />
         {children}
+        </div>
         <div className={styles.trailer}>All rights reserved. Release version: local_build</div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from './layout.module.css';
-import Navbar from "./components/Navbar";
 
 export default function Home() {
 
   return (
     <main>
-        <div className={styles.leftPlacement}>
-        <Navbar />
-        </div>
-        <div className={styles.rightPlacement}>
         <div className={styles.content}>
           <Outlet />
-        </div>
         </div>
     </main>
   );
